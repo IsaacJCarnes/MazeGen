32cb6bd88be715c36c8e1f77dcbbcfc2c5b158ef
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-10-12 20:06:40 -0400

    added code to movement functions which allows user to move up (w) left (a) down (s) and right (d), need to add conditional to block movement if cell is a wall, commented out unused code, todo- fix api call so get array of characters and allow selection of character

--- assets/javascript/character.js
+++ assets/javascript/character.js
@@ -1,57 +1,9 @@
-console.log("hello world");
+// console.log("hello world");
 
-// var testKeys = document.getElementById("testkeys")
+var blocks = document.querySelectorAll("div");
+var startPoint = document.getElementById("07 04");
 
-// testKeys.addEventListener('keydown', function(event) {
-//   console.log(event)
-//   var key = event.key.toLowerCase();
-
-//   if ("wasd".indexOf(key) >= 0) {
-//     switch (key) {
-//       case "w":
-//         console.log("Move up");
-//         break;
-//     }
-//   }
-// });
-
-// testKeys.addEventListener('keydown', function(event) {
-//   var key = event.key.toLowerCase();
-
-//   if ("wasd".indexOf(key) >= 0) {
-//     switch (key) {
-//       case "s":
-//         console.log("Move down");
-//         break;
-//     }
-//   }
-// });
-
-// testKeys.addEventListener('keydown', function(event) {
-//   var key = event.key.toLowerCase();
-
-//   if ("wasd".indexOf(key) >= 0) {
-//     switch (key) {
-//       case "a":
-//         console.log("Move left");
-//         break;
-//     }
-//   }
-// });
-
-// testKeys.addEventListener('keydown', function(event) {
-//   var key = event.key.toLowerCase();
-
-//   if ("wasd".indexOf(key) >= 0) {
-//     switch (key) {
-//       case "d":
-//         console.log("Move right");
-//         break;
-//     }
-//   }
-// });
-
-function displayCharacter() {
+function callCharacter() {
   var characterUrl = "https://acnhapi.com/v1/villagers";
 
   fetch(characterUrl)
@@ -62,57 +14,201 @@ function displayCharacter() {
       // console.log(data)
       // console.log(data.bea02.icon_uri);
       bearIcon = data.bea02.icon_uri;
-      createBearIcon(bearIcon)
+      // console.log(data.bea02.icon_uri)
+      // createBearIcon(bearIcon);
     });
 }
-displayCharacter();
+callCharacter();
 
-var startPoint = document.getElementById("00 00");
+// function createBearIcon(bearIcon) {
+  var iconImage = document.createElement("img");
+  // iconImage.setAttribute("src", bearIcon);
+  iconImage.setAttribute("src", "https://acnhapi.com/v1/icons/villagers/10");
+  iconImage.setAttribute("alt", "bear icon");
+  iconImage.setAttribute("id", "userIcon");
+  iconImage.setAttribute("class", "bear-icon");
+  startPoint.appendChild(iconImage);
+// }
 
-function createBearIcon(bearIcon) {
-var iconImage = document.createElement("img");
-iconImage.setAttribute("src", bearIcon);
-iconImage.setAttribute("alt", "bear icon");
-iconImage.setAttribute("id", "userIcon");
-iconImage.setAttribute("class", "bear-icon");
-startPoint.appendChild(iconImage);
+function moveRight() {
+  document.addEventListener("keydown", function (event) {
+    var key = event.key.toLowerCase();
+
+    if ("wasd".indexOf(key) >= 0) {
+      switch (key) {
+        case "d":
+          console.log("Move right");
+          var cellID = iconImage.parentNode.id
+          console.log(cellID);
+          var cellArray = cellID.split(" ");
+          console.log(cellArray);
+          var firstIndex = parseInt(cellArray[0])
+          console.log(firstIndex)
+          var movedToCell = firstIndex + 1
+          console.log(movedToCell)
+          var backToString = JSON.stringify(movedToCell)
+          console.log(backToString)
+          var revertedString = "0"
+          console.log(backToString.length)
+          var completedID = ""
+          if(backToString.length === 1){
+            completedID = revertedString + backToString
+          } else if (backToString.length === 2) {
+            completedID = backToString
+          }
+          console.log(completedID)
+          cellArray[0]= completedID
+          console.log(cellArray)
+          console.log(completedID)
+          var newLocation = cellArray.join(" ")
+          console.log(newLocation)
+          // console.log(document.getElementById(`${newLocation}`))
+          // // console.log(document.getElementById(`${newLocation}`).getAttribute("data-is-wall"))
+          // document.getElementById(`${newLocation}`).appendChild(iconImage)
+          // if (document.getElementById(`${newLocation}`).getAttribute("data-is-wall") === true) {
+          //   return;
+          // } else {
+            document.getElementById(`${newLocation}`).appendChild(iconImage)
+          // }
+          break;
+      }
+    }
+  });
 }
 
+function moveUp() {
+  document.addEventListener("keydown", function (event) {
+    var key = event.key.toLowerCase();
 
-function numberToStringID(numberID){ //Returns string id
-  var split = numberID.split(' ', 2);
-  var newId = '';
-  if(split[0] < 10){ //Col
-      newId = newId.concat('0' + split[0]);
-  } else{
-      newId = newId.concat(split[0]);
-  }
-  newId = newId.concat(' ');
-  if(split[1] < 0){ //Row
-      newId = newId.concat('-' + '0' + Math.abs(split[1]));
-  } else if(split[1] < 10){
-      newId = newId.concat('0' + split[1]);
-  } else{
-      newId = newId.concat(split[1]);
-  }
-  return newId;
+    if ("wasd".indexOf(key) >= 0) {
+      switch (key) {
+        case "w":
+          console.log("Move up");
+          var cellID = iconImage.parentNode.id
+          var cellArray = cellID.split(" ");
+          var secondIndex = parseInt(cellArray[1])
+          var movedToCell = secondIndex - 1
+          var backToString = JSON.stringify(movedToCell)
+          var revertedString = "0"
+          var completedID = ""
+          if(backToString.length === 1){
+            completedID = revertedString + backToString
+          } else if (backToString.length === 2) {
+            completedID = backToString
+          }
+          cellArray[1]= completedID
+          var newLocation = cellArray.join(" ")
+          console.log(newLocation)
+          // console.log(document.getElementById(`${newLocation}`))
+          // console.log(document.getElementById(`${newLocation}`).getAttribute("data-is-wall"))
+          document.getElementById(`${newLocation}`).appendChild(iconImage)
+          break;
+      }
+    }
+  });
+}
+
+function moveDown() {
+  document.addEventListener("keydown", function (event) {
+    var key = event.key.toLowerCase();
+
+    if ("wasd".indexOf(key) >= 0) {
+      switch (key) {
+        case "s":
+          console.log("Move down");
+          var cellID = iconImage.parentNode.id
+          var cellArray = cellID.split(" ");
+          var secondIndex = parseInt(cellArray[1])
+          var movedToCell = secondIndex + 1
+          var backToString = JSON.stringify(movedToCell)
+          var revertedString = "0"
+          var completedID = ""
+          if(backToString.length === 1){
+            completedID = revertedString + backToString
+          } else if (backToString.length === 2) {
+            completedID = backToString
+          }
+          cellArray[1]= completedID
+          var newLocation = cellArray.join(" ")
+          // console.log(document.getElementById(`${newLocation}`))
+          // console.log(document.getElementById(`${newLocation}`).getAttribute("data-is-wall"))
+          document.getElementById(`${newLocation}`).appendChild(iconImage)
+          break;
+      }
+    }
+  });
+}
+
+function moveLeft() {
+  document.addEventListener("keydown", function (event) {
+    var key = event.key.toLowerCase();
+
+    if ("wasd".indexOf(key) >= 0) {
+      switch (key) {
+        case "a":
+          console.log("Move left");
+          var cellID = iconImage.parentNode.id
+          var cellArray = cellID.split(" ");
+          var firstIndex = parseInt(cellArray[0])
+          var movedToCell = firstIndex - 1
+          var backToString = JSON.stringify(movedToCell)
+          var revertedString = "0"
+          var completedID = ""
+          if(backToString.length === 1){
+            completedID = revertedString + backToString
+          } else if (backToString.length === 2) {
+            completedID = backToString
+          }
+          cellArray[0]= completedID
+          var newLocation = cellArray.join(" ")
+          // console.log(document.getElementById(`${newLocation}`))
+          // console.log(document.getElementById(`${newLocation}`).getAttribute("data-is-wall"))
+          document.getElementById(`${newLocation}`).appendChild(iconImage)
+          break;
+      }
+    }
+  });
+}
+
+// function numberToStringID(numberID) {
+//   //Returns string id
+//   var split = numberID.split(" ", 2);
+//   var newId = "";
+//   if (split[0] < 10) {
+//     //Col
+//     newId = newId.concat("0" + split[0]);
+//   } else {
+//     newId = newId.concat(split[0]);
+//   }
+//   newId = newId.concat(" ");
+//   if (split[1] < 0) {
+//     //Row
+//     newId = newId.concat("-" + "0" + Math.abs(split[1]));
+//   } else if (split[1] < 10) {
+//     newId = newId.concat("0" + split[1]);
+//   } else {
+//     newId = newId.concat(split[1]);
+//   }
+//   return newId;
+// }
+
+// console.log(blocks)
+for (var i = 0; i < 19; i++) {
+  var blockData = blocks[i].getAttribute("data-is-wall");
+  console.log(blockData);
 }
 
 // function startPlacement(){
-// for(var i = 0; i < 5; i++){
+// for(var i = 0; i < 2; i++){
 //   for(var j = 0; j <= i; j++){
 //     console.log(numberToStringID(i + " " + j));
 //     console.log(numberToStringID(j + " " + i));
-//     // if(document.getElementById("").data-wall == false){
-//     //   something.appendChild(iconImage)
-//     // }
 //   }
 // }
 // }
 
-var blocks = document.querySelectorAll("div");
-console.log(blocks)
-for (var i = 0; i < 19; i++) {
-var blockData = blocks[i].getAttribute("data-is-wall")
-console.log(blockData)
-}
+// startPlacement();
+moveUp();
+moveDown();
+moveLeft();
+moveRight();
