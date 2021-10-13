b6310d3fb137e73e91f9297e293ce08287e6f990
Adriana Broadnax <alwaysadrianna@gmail.com>
2021-10-12 22:04:06 -0400

    java

---
+++ assets/javascript/landingPage.js
@@ -0,0 +1,7 @@
+var startBtn = document.querySelector("#start-button");
+const playContainer = document.getElementById("playContainer");
+
+startBtn.addEventListener("click"), function (onClick) {
+    console.log("start");
+    const playContainer = ["#playContainer"]
+}
