43bc894305fd50eadb617e9f81d629ec971651ff
Adriana Broadnax <alwaysadrianna@gmail.com>
2021-10-14 16:26:08 -0400

    java

--- assets/javascript/landingPage.js
+++ assets/javascript/landingPage.js
@@ -2,6 +2,8 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("modal-button");
 var span = document.getElementsByClassName("close")[0];
 var maze = document.getElementsByClassName("")
+var mainContainer = document.querySelector(".mainContainer")
+var giphyLink = document.querySelector("#giphy-link")
 
 // When the user clicks the button, open the modal 
 btn.addEventListener("click", function () {
@@ -29,22 +31,29 @@ startBtn.addEventListener("click", function () {
     const playContainer = ["#playContainer"]
 })
 
+giphy();
 
 function giphy(){
   console.log("Will show a giphy");
   // empty container
   mainContainer.innerHTML = "";
 
-  var giphyURL = document.createElement("img");
-  giphyURL.setAttribute("id", "giphy");
-  giphyURL.setAttribute("src", "http://api.giphy.com/v1/gifs/search?q=green+maze&api_key=Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc");
-  giphyURL.setAttribute("alt", "maze giphy");
 
-var giphyURL = "http://api.giphy.com/v1/gifs/search?q=green+maze&api_key=Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc"
-fetch(giphyURL)
-.then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data.data[1].images.original.url)
-  })}
+
+  var giphyURL = "http://api.giphy.com/v1/gifs/search?q=green+maze&api_key=Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc"
+  fetch(giphyURL)
+  .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.data[1].images.original.url)
+      var giphyURL = document.createElement("img");
+      giphyURL.setAttribute("id", "giphy");
+      // giphyURL.setAttribute("src", "http://api.giphy.com/v1/gifs/search?q=green+maze&api_key=Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc");
+      giphyURL.setAttribute("alt", "maze giphy");
+      giphyURL.setAttribute("src", data.data[1].images.original.url);
+      giphyLink.setAttribute("href", data.data[1].images.original.url);
+      giphyLink.appendChild(giphyURL);
+
+    })
+}
