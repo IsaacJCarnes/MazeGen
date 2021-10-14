8a408ec9d1aee03e011a7ef33d6d2ef03561f869
Adriana Broadnax <alwaysadrianna@gmail.com>
2021-10-13 21:01:25 -0400

    java

--- assets/javascript/landingPage.js
+++ assets/javascript/landingPage.js
@@ -1,6 +1,7 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("modal-button");
 var span = document.getElementsByClassName("close")[0];
+var maze = document.getElementsByClassName("image")
 
 // When the user clicks the button, open the modal 
 btn.addEventListener("click", function () {
@@ -28,11 +29,35 @@ startBtn.addEventListener("click", function () {
     const playContainer = ["#playContainer"]
 })
 
-var giphyURL = "api.giphy.com/v1/gifs/Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc/{XBpUGMmoGM4DVHoRMZ}"
+// var giphyURL = "api.giphy.com/v1/gifs/Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc/{ecf05e476vqxvb7rpq4mkbel0b2nu5s97gyf9z2o0ueki1g4}"
+// fetch(giphyURL)
+// .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data)
+//   })
+
+// var giphyURL = "api.giphy.com/v1/gifs/search?q=green+maze&api_key=Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc"
+// fetch(giphyURL)
+// .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data)
+//   })
+
+  //api.giphy.com/v1/gifs/search?q=green+maze&api_key=Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc
+  // http:api.giphy.com/v1/gifs/search?q=green+maze&api_key=Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc
+
+ 
+
+var giphyURL = "http://api.giphy.com/v1/gifs/search?q=green+maze&api_key=Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc"
 fetch(giphyURL)
 .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data)
+    console.log(data.data[1].images.original.url)
   })
+
