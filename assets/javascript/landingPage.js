17cfde37bd8eda36f062552011df5e1baa4ede00
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-10-19 19:01:06 -0400

    added div with elements to serve as modal when highscore button is clicked instead of relocating to hs page, styled highscore modal content, formated landingPage JS and also added new variables and functions for highscore modal, deleted commented out code and added function to get highscores and display them in modal

--- assets/javascript/landingPage.js
+++ assets/javascript/landingPage.js
@@ -1,74 +1,82 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("modal-button");
- var span = document.getElementsByClassName("close")[0];
- var maze = document.getElementsByClassName("")
- var mainContainer = document.querySelector(".mainContainer")
- var giphyLink = document.querySelector("#giphy-link")
- // var giphyLink = document.querySelector("#giphy-link")
+var instructionModal = document.getElementById("instructionModal");
+var hsModal = document.getElementById("highScoreModal");
 
- // When the user clicks the button, open the modal 
- btn.addEventListener("click", function () {
-  modal.style.display = "block";
-})
+var instructionBtn = document.getElementById("modal-button");
+var hsBtn = document.getElementById("highScore-button");
+
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+
+var maze = document.getElementsByClassName("");
+var mainContainer = document.querySelector(".mainContainer");
+var giphyLink = document.querySelector("#giphy-link");
+// var giphyLink = document.querySelector("#giphy-link")
+
+// When the user clicks the button, open the modal
+instructionBtn.addEventListener("click", function () {
+  instructionModal.style.display = "block";
+});
+
+hsBtn.addEventListener("click", function () {
+  hsModal.style.display = "block";
+});
+
 // When the user clicks on <span> (x), close the modal
 span.addEventListener("click", function () {
-  modal.style.display = "none";
-})
+  instructionModal.style.display = "none";
+});
+span2.addEventListener("click", function () {
+  console.log("clicked");
+  hsModal.style.display = "none";
+});
+
 // When the user clicks anywhere outside of the modal, close it
-window.addEventListener("click", function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.addEventListener("click", function (event) {
+  if (event.target == instructionModal || event.target == hsModal) {
+    instructionModal.style.display = "none";
+    hsModal.style.display = "none";
   }
-})
+});
+
 var startBtn = document.querySelector("#start-button");
 const playContainer = document.getElementById("playContainer");
 startBtn.addEventListener("click", function () {
-    console.log("start");
-    document.location = "mazePage.html"
-    // const playContainer = ["#playContainer"]
-})
-
-var highScoreBtn = document.querySelector("#highScore-button");
-highScoreBtn.addEventListener("click", function () {
-  console.log("highscore");
-  document.location = "highScore.html"
-  displayHighScores();
-})
+  console.log("start");
+  document.location = "mazePage.html";
+  // const playContainer = ["#playContainer"]
+});
 
 giphy();
-function giphy(){
-  var giphyURL = "https://api.giphy.com/v1/gifs/search?q=green+maze&api_key=Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc"
+function giphy() {
+  var giphyURL =
+    "https://api.giphy.com/v1/gifs/search?q=green+maze&api_key=Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc";
   fetch(giphyURL)
-  .then(function (response) {
+    .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       var gifImg = document.getElementById("mazeGif");
       var gifURL = data.data[1].images.original.url;
       gifImg.setAttribute("src", gifURL);
-    })
+    });
 }
 
-// function giphy(){
-//   console.log("Will show a giphy");
-//   // empty container
-//   mainContainer.innerHTML = "";
-//   var giphyURL = "http://api.giphy.com/v1/gifs/search?q=green+maze&api_key=Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc"
-//   fetch(giphyURL)
-//   .then(function (response) {
-//       return response.json();
-//      })
-//      .then(function (data) {
-//        console.log(data.data[1].images.original.url)
-//        var giphyLink = document.createElement("a")
-//        var giphyURL = document.createElement("img");
-//        giphyURL.setAttribute("id", "giphy");
-//        // giphyURL.setAttribute("src", "http://api.giphy.com/v1/gifs/search?q=green+maze&api_key=Y7ydLBRXvMZyDQOIVNwjMcWyI3mJmThc");
-//        giphyURL.setAttribute("alt", "maze giphy");
-//        giphyURL.setAttribute("src", data.data[1].images.original.url);
-//        giphyLink.setAttribute("href", data.data[1].images.original.url);
-//        mainContainer.appendChild(giphyLink)
-//        giphyLink.appendChild(giphyURL);
+function getHighscoresFromLocalStorage() {
+  return JSON.parse(localStorage.getItem("highScores")) || [];
+}
+var highScoreModalContainer = document.querySelector("#hs-modal-container");
+function renderHighScores() {
+  var allHighScores = getHighscoresFromLocalStorage();
+  highScoreModalContainer.innerHTML = "";
 
-//      })
-// 
+  for (var i = 0; i < allHighScores.length; i++) {
+    var highScoreName = allHighScores[i].userName;
+    var highScore = allHighScores[i].score;
+
+    var li = document.createElement("li");
+    li.setAttribute("data-index", i);
+    highScoreModalContainer.appendChild(li);
+    li.textContent = `${highScoreName}: ${highScore} seconds`;
+  }
+}
+renderHighScores();
